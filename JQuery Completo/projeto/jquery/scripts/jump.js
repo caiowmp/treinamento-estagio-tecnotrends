ce1d68e11e70de59e4b6d20cf7d11a9ce1d4e219
caiowmp <contato.caiomp@gmail.com>
2022-10-07 10:21:59 -0300

    Criando um plugin

---
+++ JQuery Completo/projeto/jquery/scripts/jump.js
@@ -0,0 +1,20 @@
+(function($){
+    $.fn.jump = function () {
+        $(this)
+            .finish()
+            .animate({
+                'marginTop': '-35px'
+            }, 150)
+            .animate({
+                'marginTop': '0px'
+            }, 150)
+            .animate({
+                'marginTop': '-35px'
+            }, 150)
+            .animate({
+                'marginTop': '0px'
+            }, 150);
+        
+        return this;
+    }
+})(JQuery)
